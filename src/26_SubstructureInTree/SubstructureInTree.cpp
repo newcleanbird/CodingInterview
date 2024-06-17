1a178c3240ec71fd1e46d95e18804728b9e6bc0a
newcleanbird <dante.song@foxmail.com>
2024-06-17 21:44:11 +0800

    init Q27

--- src/26_SubstructureInTree/SubstructureInTree.cpp
+++ src/26_SubstructureInTree/SubstructureInTree.cpp
@@ -1,19 +1,11 @@
-/*******************************************************************
-Copyright(c) 2016, Harry He
-All rights reserved.
-
-Distributed under the BSD license.
-(See accompanying file LICENSE.txt at
-https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
-*******************************************************************/
-
 //==================================================================
-// ¡¶½£Ö¸Offer¡ª¡ªÃûÆóÃæÊÔ¹Ù¾«½²µäĞÍ±à³ÌÌâ¡·´úÂë
-// ×÷Õß£ººÎº£ÌÎ
+// ã€Šå‰‘æŒ‡Offerâ€”â€”åä¼é¢è¯•å®˜ç²¾è®²å…¸å‹ç¼–ç¨‹é¢˜ã€‹ä»£ç 
+// æœ¬ä¹¦åŸä½œè€…ï¼šä½•æµ·æ¶›
+// ä»“åº“ä½œè€…ï¼šæ·¡å®šä½†ä¸
 //==================================================================
 
-// ÃæÊÔÌâ26£ºÊ÷µÄ×Ó½á¹¹
-// ÌâÄ¿£ºÊäÈëÁ½¿Ã¶ş²æÊ÷AºÍB£¬ÅĞ¶ÏBÊÇ²»ÊÇAµÄ×Ó½á¹¹¡£
+// é¢è¯•é¢˜26ï¼šæ ‘çš„å­ç»“æ„
+// é¢˜ç›®ï¼šè¾“å…¥ä¸¤æ£µäºŒå‰æ ‘Aå’ŒBï¼Œåˆ¤æ–­Bæ˜¯ä¸æ˜¯Açš„å­ç»“æ„ã€‚
 
 #include <cstdio>
 
@@ -67,7 +59,7 @@ bool Equal(double num1, double num2)
         return false;
 }
 
-// ====================¸¨Öú²âÊÔ´úÂë====================
+// ====================è¾…åŠ©æµ‹è¯•ä»£ç ====================
 BinaryTreeNode* CreateBinaryTreeNode(double dbValue)
 {
     BinaryTreeNode* pNode = new BinaryTreeNode();
@@ -102,7 +94,7 @@ void DestroyTree(BinaryTreeNode* pRoot)
     }
 }
 
-// ====================²âÊÔ´úÂë====================
+// ====================æµ‹è¯•ä»£ç ====================
 void Test(char* testName, BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2, bool expected)
 {
     if(HasSubtree(pRoot1, pRoot2) == expected)
@@ -111,7 +103,7 @@ void Test(char* testName, BinaryTreeNode* pRoot1, BinaryTreeNode* pRoot2, bool e
         printf("%s failed.\n", testName);
 }
 
-// Ê÷ÖĞ½áµãº¬ÓĞ·Ö²æ£¬Ê÷BÊÇÊ÷AµÄ×Ó½á¹¹
+// æ ‘ä¸­ç»“ç‚¹å«æœ‰åˆ†å‰ï¼Œæ ‘Bæ˜¯æ ‘Açš„å­ç»“æ„
 //                  8                8
 //              /       \           / \
 //             8         7         9   2
@@ -145,7 +137,7 @@ void Test1()
     DestroyTree(pNodeB1);
 }
 
-// Ê÷ÖĞ½áµãº¬ÓĞ·Ö²æ£¬Ê÷B²»ÊÇÊ÷AµÄ×Ó½á¹¹
+// æ ‘ä¸­ç»“ç‚¹å«æœ‰åˆ†å‰ï¼Œæ ‘Bä¸æ˜¯æ ‘Açš„å­ç»“æ„
 //                  8                8
 //              /       \           / \
 //             8         7         9   2
@@ -179,7 +171,7 @@ void Test2()
     DestroyTree(pNodeB1);
 }
 
-// Ê÷ÖĞ½áµãÖ»ÓĞ×ó×Ó½áµã£¬Ê÷BÊÇÊ÷AµÄ×Ó½á¹¹
+// æ ‘ä¸­ç»“ç‚¹åªæœ‰å·¦å­ç»“ç‚¹ï¼Œæ ‘Bæ˜¯æ ‘Açš„å­ç»“æ„
 //                8                  8
 //              /                   / 
 //             8                   9   
@@ -215,7 +207,7 @@ void Test3()
     DestroyTree(pNodeB1);
 }
 
-// Ê÷ÖĞ½áµãÖ»ÓĞ×ó×Ó½áµã£¬Ê÷B²»ÊÇÊ÷AµÄ×Ó½á¹¹
+// æ ‘ä¸­ç»“ç‚¹åªæœ‰å·¦å­ç»“ç‚¹ï¼Œæ ‘Bä¸æ˜¯æ ‘Açš„å­ç»“æ„
 //                8                  8
 //              /                   / 
 //             8                   9   
@@ -251,7 +243,7 @@ void Test4()
     DestroyTree(pNodeB1);
 }
 
-// Ê÷ÖĞ½áµãÖ»ÓĞÓÒ×Ó½áµã£¬Ê÷BÊÇÊ÷AµÄ×Ó½á¹¹
+// æ ‘ä¸­ç»“ç‚¹åªæœ‰å³å­ç»“ç‚¹ï¼Œæ ‘Bæ˜¯æ ‘Açš„å­ç»“æ„
 //       8                   8
 //        \                   \ 
 //         8                   9   
@@ -287,7 +279,7 @@ void Test5()
     DestroyTree(pNodeB1);
 }
 
-// Ê÷AÖĞ½áµãÖ»ÓĞÓÒ×Ó½áµã£¬Ê÷B²»ÊÇÊ÷AµÄ×Ó½á¹¹
+// æ ‘Aä¸­ç»“ç‚¹åªæœ‰å³å­ç»“ç‚¹ï¼Œæ ‘Bä¸æ˜¯æ ‘Açš„å­ç»“æ„
 //       8                   8
 //        \                   \ 
 //         8                   9   
@@ -324,7 +316,7 @@ void Test6()
     DestroyTree(pNodeB1);
 }
 
-// Ê÷AÎª¿ÕÊ÷
+// æ ‘Aä¸ºç©ºæ ‘
 void Test7()
 {
     BinaryTreeNode* pNodeB1 = CreateBinaryTreeNode(8);
@@ -340,7 +332,7 @@ void Test7()
     DestroyTree(pNodeB1);
 }
 
-// Ê÷BÎª¿ÕÊ÷
+// æ ‘Bä¸ºç©ºæ ‘
 void Test8()
 {
     BinaryTreeNode* pNodeA1 = CreateBinaryTreeNode(8);
@@ -356,7 +348,7 @@ void Test8()
     DestroyTree(pNodeA1);
 }
 
-// Ê÷AºÍÊ÷B¶¼Îª¿Õ
+// æ ‘Aå’Œæ ‘Béƒ½ä¸ºç©º
 void Test9()
 {
     Test("Test9", nullptr, nullptr, false);
